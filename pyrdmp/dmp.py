f70b19a45710e59a524666a37f6f754ba85954cc
jbd95 <jbrady9559@gmail.com>
2019-01-24 18:23:55 -0600

    Update dmp.py PEP 8 conventions

--- pyrdmp/dmp.py
+++ pyrdmp/dmp.py
@@ -1,61 +1,59 @@
-import numpy as np
-
 '''
     Dynamic Movement Primitive Class
     Author: Michail Theofanidis
 '''
-class DynamicMovementPrimative:
 
-    '''
-        a: Gain a of the transformation system
-        b: Gain b of the transformation system
-        asDeg: Degredation of the canonical system
-        ng: Number of Gaussians
-        stb: Stablilization term
-    '''
+import numpy as np
+
+class DynamicMovementPrimative:
+    """Create an DMP.
+        
+        Keyword arguments:
+        a -- Gain a of the transformation system
+        b -- Gain b of the transformation system
+        as_deg -- Degredation of the canonical system
+        ng -- Number of Gaussians
+        stb -- Stablilization term
+    """
+
     def __init__(self, _a, _b, _as, _ng, _stb):
        
         self.a = _a
         self.b = _b
-        self.asDeg = _as
+        self.as_deg = _as
        
         if _ng < 2:
             raise Exception('Number of gaussians must be greater than 1.')
         self.ng = _ng
         self.stb = _stb
 
-    # Create the phase of the system
     def phase(self, time):
-         return np.exp((-self.asDeg) * np.linspace(0, 1.0, len(time))).T
+        """Create the phase of the system using the time vector."""
 
-    # Generate Gaussian Distributions
+         return np.exp((-self.as_deg) * np.linspace(0, 1.0, len(time))).T
+
     def distributions(self, s):
-        raise Exception('Function not implemented yet');
-        '''
-        increment = (s[0] - s[len(s) - 1]) / (self.ng - 1);
-        center = range(s[len(s) - 1], s[0], increment)
-        lrCenter = np.fliplr(center)
-        d = np.diff(center)
-        c /= d[0]
-        '''
+        """Generate the gaussian distributions."""
+        raise Exception('Function not implemented yet')
    
-    # Normalize a vector, returns a numpy array with equal distribution between elements
     @staticmethod
-    def normalizeVector(v):
+    def normalize_vector(v):
+        """Returns a normalized numpy array"""
         return np.linspace(0, v[len(v) - 1], len(v))
 
     # Get the time and joint vectors from the demonstration data
     @staticmethod
-    def parseDemo(data):
-        #return time vector, joint angles vector
+    def parse_demo(data):
+        """Return the time vector and the joint angles vector"""
         return data[:, 0], data[:, 1:7]
 
-    # Load in time vector and joint positions for a given demonstration
     @staticmethod
-    def loadDemo(fileName):
+    def load_demo(fileName):
+        """Load the data contained in the given file name."""
         return np.loadtxt(fileName, dtype=float, delimiter=',', skiprows=1)
 
     # Generate a Gaussian using its height, center, and the phase
     @staticmethod
-    def psiF(height, center, state):
+    def psif(height, center, state):
+        """Calculate psi (gaussian) based on its height, center, and state"""
         return np.exp((-height)*(np.power(state-center, 2)))
