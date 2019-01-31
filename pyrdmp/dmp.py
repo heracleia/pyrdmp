936d1881384751ce0ce6fa4558091c32cd208af7
MichailTheofanidis <michailtheofanidis@hotmail.com>
2019-01-31 16:44:16 -0600

    Imitation and Trajectory generation implementation

--- pyrdmp/dmp.py
+++ pyrdmp/dmp.py
@@ -34,6 +34,110 @@ class DynamicMovementPrimitive:
     def distributions(self, s):
         raise Exception('Function not implemented yet')
 
+    # Imitation Learning
+    def imitate(self, x, dx, ddx, time, s, psv):
+
+        # Initialize variables
+        sigma = np.zeros(len(time))
+        f_target = np.zeros(len(time))
+        w = np.zeros(self.ng)
+
+        g = x[-1]
+        x0 = x[0]
+        tau = time[-1]
+
+        # Compute ftarget
+        for i in range(0, len(time)):
+
+            # Add stabilization term
+            if self.stb == 1:
+                mod = np.multiply(self.b*(g-x0), s[i])
+                sigma[i] = np.multiply(g-x0, s[i])
+            else:
+                mod = 0
+                sigma[i] = s[i]
+
+            f_target[i] = np.multiply(np.power(tau, 2), ddx[i]) - self.a*(self.b*(g-x[i])-tau*dx[i])+mod
+
+        # Regression
+        for i in range(0, self.ng):
+
+            w[i] = np.divide(np.transpose(sigma)*np.diagonal(psv[i][:])*f_target,np.transpose(sigma)*np.diagonal(psv[i][:])*sigma)
+
+        return f_target, w
+
+    # Generate a trajectory
+    def generate(self, w, x0, g, time, s, psv):
+
+        # Initialize variables
+        ddx = np.zeros(len(time))
+        dx = np.zeros(len(time))
+        x = np.zeros(len(time))
+        sigma = np.zeros(len(time))
+        f_rep = np.zeros(len(time))
+
+        tau = time[-1]
+        x[0] = x0
+
+        dx_r = 0
+        x_r = 0
+
+        for i in range(0, len(time)):
+
+            p_sum = 0
+            p_div = 0
+
+            if i == 1:
+                dt = time[i]
+            else:
+                dt = time[i] - time[i - 1]
+
+            # Add stabilization term
+            if self.stb == 1:
+                mod = np.multiply(self.b*(g-x0), s[i])
+                sigma[i] = np.multiply(g-x0, s[i])
+            else:
+                mod = 0
+                sigma[i] = s[i]
+
+            for j in range(0, self.ng):
+
+                p_sum = p_sum + np.multiply(psv[j][i], w[j])
+                p_div = p_div + psv[j][i]
+
+            # Calculate the new control input
+            f_rep[i] = np.multiply(np.divide(p_sum, p_div), sigma[i])
+
+            # Calculate the new trajectory
+            ddx_r = np.divide(self.a*(self.b*(g-x_r)-tau*dx_r)+f_rep[i]+mod, np.power(tau, 2))
+            dx_r = dx_r + np.multiply(ddx_r, dt)
+            x_r = x_r + np.multiply(dx_r, dt)
+
+            ddx[i] = ddx_r
+            dx[i] = dx_r
+            x[i] = x_r
+
+        return ddx, dx, x
+
+    """"
+    # Adaptation using reinforcement learning
+    def adapt(self, w, x0, g, time, s, psv, samples, rate):
+
+        # Intialize the action variables
+        actions = np.ones(samples, self.ng)
+        exploration = np.zeros(samples, self.ng)
+        a=w
+
+        # Flag which acts as a stop condition
+        flag=0
+
+        while flag==0:
+
+            for i in range(0, samples):
+                for j in range(0, self.ng):
+                    exploration[i][j]=
+    """
+
     # Function that smooths a trajectory
     # a: NumPy 1-D array containing the data to be smoothed.
     # window: smoothing window size needs, which must be odd number.
